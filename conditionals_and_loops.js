d328271c663723a6c1a2ba98912ae54e8d2c7acc
danizaghian <dani.zaghian@gmail.com>
2017-10-03 20:03:24 -0700

    conditionals and loops

---
+++ conditionals_and_loops.js
@@ -0,0 +1,103 @@
+// RUBY CONDITIONALS
+// if x > 50
+//  puts "High number"
+// elsif x < 25
+//  puts "Low Number"
+// elsif x == 32
+//  puts "My favorite number"
+// else
+//  puts "Medium Number"
+// end
+
+// JS CONDITIONALS
+// var x = 31;
+// if (x > 50) {
+//   console.log("High number");
+// } else if (x < 25) {
+//   console.log("Low number");
+// } else if (x === 31) {
+//   console.log("My favorite number");
+// } else {
+//   console.log("Medium number");
+// }
+
+//RUBY EQUALITY
+// 5 == 5 //true
+// 5 == "5" //false
+// 5 != "5" //true
+
+// JS EQUALITY
+// console.log(5 == 5); //true
+// console.log(5 == "5"); //true
+// console.log(5 === "5"); //false
+// console.log(5 !== "5"); //true
+// console.log(5 != "5"); //false
+
+// RUBY RANDOM NUMBER
+// rand(1..10)
+
+// JS RANDOM NUMBER
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// RUBY WHILE LOOPS
+// i = 0
+// text = ""
+// while i < 10
+//  text = text + " the number is " + i
+//  i += 1
+// end
+// puts text
+
+// JS WHILE LOOPS
+// var i = 0;
+// var text = "";
+// while (i < 10) {
+//   text = text + " the number is " + i;
+//   i++;
+// }
+// console.log(text);
+
+// RUBY FOR LOOP
+// for i in 0...10
+//  puts i
+// end
+
+// JS FOR LOOP
+// for (var i = 1; i < 20; i+=2) {
+//   console.log(i);
+// }
+
+// IN ENGLISH
+// for an arbritrary variable called i, set it to 1
+// continue running the lop as long as i is less than 20
+// each time the loop runs, increase i by 2
+
+// RUBY EACH THROUGH
+// words = ["apple", "banana", "carrot", "date", "fig"]
+// words.each do |word|
+//  puts word
+// end
+
+// JS EACH THROUGH
+// var words = ["apple", "banana", "carrot", "date", "fig"];
+// for (var i = 0; i < words.length; i++) {
+//   console.log(words[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
